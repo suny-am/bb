44835293d622de49cb8b2b7e641162a37334a4e8
suny-am <visualarea.1@gmail.com>
2024-09-17 14:50:57 +0000

    add basic commit cmd

--- pkg/cmd/commit.go
+++ pkg/cmd/commit.go
@@ -4,10 +4,17 @@ Copyright © 2024 Calle Sandberg <visualarea.1@gmail.com>
 package cmd
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"os"
 
+	"github.com/go-resty/resty/v2"
+	"github.com/joho/godotenv"
 	"github.com/spf13/cobra"
 )
+
+var Commit string
 
 // commitCmd represents the commit command
 var commitCmd = &cobra.Command{
@@ -16,7 +23,65 @@ var commitCmd = &cobra.Command{
 	Long: `Use this command to get commit activity information
 	from either public or workspace repositories.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("commit called")
+		err := godotenv.Load()
+		if err != nil {
+			fmt.Printf("Error loading .env file")
+		}
+
+		username := os.Getenv("BITBUCKET_USERNAME")
+		appPassword := os.Getenv("BITBUCKET_APP_PASSWORD")
+		credentials := fmt.Sprintf("%s:%s", username, appPassword)
+		b64 := base64.StdEncoding.EncodeToString([]byte(credentials))
+		authHeaderData := fmt.Sprintf("Basic %s", b64)
+
+		client := resty.New()
+
+		// TBD add workspace as argument
+
+		if Workspace == "" {
+			fmt.Println("--workspace required")
+			return
+		}
+
+		if Repository == "" {
+			fmt.Println("--repository required")
+			return
+		}
+
+		if Commit == "" {
+			fmt.Println("--commit required")
+			return
+		}
+
+		endpoint := fmt.Sprintf("https://api.bitbucket.org/2.0/repositories/%s/%s/commit/%s", Workspace, Repository, Commit)
+
+		fmt.Println(endpoint)
+
+		resp, err := client.R().
+			SetHeader("Authorization", authHeaderData).
+			SetHeader("Accept", "application/json").
+			EnableTrace().
+			Get(endpoint)
+
+		if resp.IsError() {
+			fmt.Println(err.Error())
+		}
+
+		if resp.IsSuccess() {
+			var data map[string]interface{}
+
+			if err := json.Unmarshal([]byte(resp.String()), &data); err != nil {
+				fmt.Println(err)
+			}
+
+			output, err := json.MarshalIndent(data, "", "  ")
+
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			fmt.Println(string(output))
+		}
 	},
 }
 
@@ -32,4 +97,7 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// commitCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	commitCmd.Flags().StringVarP(&Workspace, "workspace", "w", "", "Workspace for the repository")
+	commitCmd.Flags().StringVarP(&Repository, "repository", "r", "", "Repository for the commit")
+	commitCmd.Flags().StringVarP(&Commit, "commit", "c", "", "Target commit")
 }
