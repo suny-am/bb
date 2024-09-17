824b893b6b1a0710ac2d10086486c542333a2af9
suny-am <visualarea.1@gmail.com>
2024-09-17 08:37:43 +0000

    use unmarshal->marshal for JSON parsing

--- pkg/cmd/repo.go
+++ pkg/cmd/repo.go
@@ -15,6 +15,7 @@ import (
 )
 
 // Flag variables
+var Repository string
 var Workspace string
 
 // repoCmd represents the repo command
@@ -24,6 +25,7 @@ var repoCmd = &cobra.Command{
 	Long: `Use this command to get general information about public or
 	workspace repositories.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		// TBD: get system env instead?
 		err := godotenv.Load()
 		if err != nil {
 			fmt.Printf("Error loading .env file")
@@ -37,7 +39,18 @@ var repoCmd = &cobra.Command{
 
 		client := resty.New()
 
-		endpoint := "https://api.bitbucket.org/2.0/repositories/avupublicapis" // TBD add workspace as argument
+		// TBD add workspace as argument
+
+		endpoint := "https://api.bitbucket.org/2.0/repositories"
+
+		if Workspace != "" {
+			endpoint = fmt.Sprintf("%s/%s", endpoint, Workspace)
+
+			// --repository requires --workspace
+			if Repository != "" {
+				endpoint = fmt.Sprintf("%s/%s", endpoint, Repository)
+			}
+		}
 
 		resp, err := client.R().
 			SetHeader("Authorization", authHeaderData).
@@ -50,45 +63,21 @@ var repoCmd = &cobra.Command{
 		}
 
 		if resp.IsSuccess() {
-			var dat map[string]interface{}
+			var data map[string]interface{}
 
-			if err := json.Unmarshal([]byte(resp.String()), &dat); err != nil {
+			if err := json.Unmarshal([]byte(resp.String()), &data); err != nil {
 				fmt.Println(err)
 			}
 
-			repositories := dat["values"].([]interface{})
+			output, err := json.MarshalIndent(data, "", "  ")
 
-			for repoIdx := range repositories {
-				repo := repositories[repoIdx].(map[string]interface{})
-
-				// links := repo["links"].(map[string]interface{})
-				// for linkIdx := range links {
-				// 	l, ok := links[linkIdx].(map[string]interface{})
-				// 	if ok {
-				// 		fmt.Printf("Link: %s\n", l["href"])
-				// 	}
-				// 	if !ok {
-				// 		l, ok := links[linkIdx].([]interface{})
-				// 		if ok {
-				// 			for i := range l {
-				// 				ref := l[i]
-				// 				ll, ok := ref.(map[string]interface{})
-				// 				if ok {
-				// 					fmt.Printf("\tLink: %s\n", ll["href"])
-				// 				}
-				// 			}
-				// 		}
-				// 	}
-				// }
-
-				fmt.Println(repo["uuid"])
-				fmt.Println(repo["name"])
-				owner := repo["owner"].(map[string]interface{})["username"]
-				fmt.Printf("Owner: %s\n", owner)
-				fmt.Printf("Size: %f Bytes\n\n", repo["size"].(float64))
+			if err != nil {
+				fmt.Println(err)
 			}
-		}
 
+			fmt.Println(string(output))
+
+		}
 	},
 }
 
@@ -105,5 +94,6 @@ func init() {
 	// is called directly, e.g.:
 	// repoCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 	repoCmd.Flags().StringVarP(&Workspace, "workspace", "w", "", "workspace name")
+	repoCmd.Flags().StringVarP(&Repository, "repository", "r", "", "repository name")
 
 }
--- pkg/cmd/user.go
+++ pkg/cmd/user.go
@@ -4,10 +4,17 @@ Copyright © 2024 Calle Sandberg <visualarea.1@gmail.com>
 package cmd
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"os"
 
+	"github.com/go-resty/resty/v2"
+	"github.com/joho/godotenv"
 	"github.com/spf13/cobra"
 )
+
+var User string
 
 // userCmd represents the user command
 var userCmd = &cobra.Command{
@@ -16,7 +23,48 @@ var userCmd = &cobra.Command{
 	Long: `Use this command to get general information about one or more
 	Bitbucket users.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("user called")
+		err := godotenv.Load()
+		if err != nil {
+			fmt.Printf("Error loading .env file")
+		}
+
+		username := os.Getenv("BITBUCKET_USERNAME")
+		appPassword := os.Getenv("BITBUCKET_APP_PASSWORD")
+		credentials := fmt.Sprintf("%s:%s", username, appPassword)
+		b64 := base64.StdEncoding.EncodeToString([]byte(credentials))
+		authHeaderData := fmt.Sprintf("Basic %s", b64)
+
+		client := resty.New()
+
+		// TBD add workspace as argument
+
+		endpoint := "https://api.bitbucket.org/2.0/user"
+
+		if User != "" {
+			endpoint = fmt.Sprintf("https://api.bitbucket.org/2.0/user/%s", User)
+		}
+
+		resp, err := client.R().
+			SetHeader("Authorization", authHeaderData).
+			SetHeader("Accept", "application/json").
+			EnableTrace().
+			Get(endpoint)
+
+		if resp.IsError() {
+			fmt.Println(err.Error())
+		}
+
+		if resp.IsSuccess() {
+			var data map[string]interface{}
+
+			if err := json.Unmarshal([]byte(resp.String()), &data); err != nil {
+				fmt.Println(err)
+			}
+
+			output, _ := json.MarshalIndent(data, "", "  ")
+
+			fmt.Println(string(output))
+		}
 	},
 }
 
@@ -32,4 +80,5 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// userCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	userCmd.Flags().StringVarP(&User, "user", "u", "", "Username registered on Bitbucket")
 }
