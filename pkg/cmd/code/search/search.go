0a4eb3b6c17748c62b7479a493f451c7039a0016
suny-am <visualarea.1@gmail.com>
2024-10-22 16:17:28 +0200

    implement pager component for displaying code search results

--- pkg/cmd/code/search/search.go
+++ pkg/cmd/code/search/search.go
@@ -2,9 +2,14 @@ package search
 
 import (
 	"errors"
+	"fmt"
+	"strings"
 
+	"github.com/charmbracelet/glamour"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/spf13/cobra"
 	"github.com/suny-am/bb/internal/keyring"
+	"github.com/suny-am/bb/internal/pager"
 )
 
 type SearchOptions struct {
@@ -24,6 +29,10 @@ var SearchCmd = &cobra.Command{
 	Long:  `Search for code in a workspace or repository`,
 
 	RunE: func(cmd *cobra.Command, args []string) error {
+		hlFg := lipgloss.AdaptiveColor{Light: "#000", Dark: "#fff"}
+		hlBg := lipgloss.AdaptiveColor{Light: "#ffbb99", Dark: "#aa11dd"}
+		mdStyle := lipgloss.NewStyle().Foreground(hlFg).Background(hlBg)
+
 		if len(args) < 1 {
 			return errors.New("<searchParam> required")
 		}
@@ -35,18 +44,59 @@ var SearchCmd = &cobra.Command{
 		opts.searchParam = args[0]
 		opts.credentials = cmd.Context().Value(keyring.CredentialsKey{}).(string)
 
-		/*
-			code, err := searchCode(&opts)
-			if err != nil {
-				fmt.Println(err)
-			}
-				for _, c := range code.Values {
-					markdown.Render(fmt.Sprintf("- **%s**\n- ```%s````", c.File.Path, c.File.Links.Html.Href))
-					if opts.includeSource {
-						markdown.Render(c.File.Source)
+		code, err := searchCode(&opts)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		pageData := []string{}
+
+		for _, c := range code.Values {
+
+			var ssb strings.Builder
+
+			fileType := strings.Split(c.File.Path, ".")
+
+			ext := fileType[len(fileType)-1]
+
+			ssb.WriteString(fmt.Sprintf("- Path: %s\n", c.File.Path))
+			ssb.WriteString(fmt.Sprintf("- Type: %s\n", ext))
+			ssb.WriteString(fmt.Sprintf("- Link: %s\n\n", c.File.Links.Html.Href))
+
+			if c.Content_match_count > 0 {
+				for i, cm := range c.Content_matches {
+
+					ssb.WriteString(fmt.Sprintf("- Match #%d:\n", i))
+					for _, l := range cm.Lines {
+						var sssb strings.Builder
+						for _, s := range l.Segments {
+							var segment string
+							if s.Match {
+								segment = mdStyle.Render(s.Text)
+							} else {
+								segment = s.Text
+							}
+							sssb.WriteString(segment)
+						}
+						ssb.WriteString(fmt.Sprintf("%s\n", sssb.String()))
 					}
+					ssb.WriteString("\n")
 				}
-		*/
+			}
+
+			if opts.includeSource {
+				r, _ := glamour.NewTermRenderer(glamour.WithAutoStyle())
+				source, err := r.Render(c.File.Source)
+				if err == nil {
+					ssb.WriteString(fmt.Sprintf("- Source: \n%s", source))
+				}
+			}
+
+			pageData = append(pageData, ssb.String())
+		}
+
+		pager.Draw(pageData)
+
 		return nil
 	},
 }
